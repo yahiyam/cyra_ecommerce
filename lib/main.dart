3c08ef5bd40dfb333d257e8b3808ec396c9f3112
yahiyam~ <yahiyamuhammed00@gmail.com>
2024-02-08 12:56:18 +0530

    Cart Provider Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:cyra_ecommerce/constants.dart';
 import 'package:cyra_ecommerce/login.dart';
+import 'package:cyra_ecommerce/provider/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   pref = await SharedPreferences.getInstance();
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cyra Ecommerce',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: mainColor),
---
+++ lib/models/cart.dart
@@ -0,0 +1,39 @@
+class CartModel {
+  int? id;
+  String? name;
+  double price;
+  double qty;
+  String? image;
+
+  CartModel({
+    this.id,
+    this.name,
+    required this.price,
+    required this.qty,
+    this.image,
+  });
+
+  factory CartModel.fromJson(Map<String, dynamic> json) => CartModel(
+        id: json["id"],
+        name: json["name"],
+        price: json["price"]?.toDouble(),
+        image: json["image"],
+        qty: json["qty"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "price": price,
+        "image": image,
+        "qty": qty,
+      };
+
+  void increase() {
+    qty++;
+  }
+
+  void decrease() {
+    qty--;
+  }
+}
